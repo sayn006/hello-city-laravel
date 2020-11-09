30a22c8e5aa1eb403c49f73a3044e4807dbe4dce
sayn006 <sayn.punithan@gmail.com>
2020-11-09 14:21:17 +0100

    make use of asset function help to load images

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,7 +3,7 @@
 @section('title')
 
 @section('content')
-<img src="/images/flag_france.png" alt="France Flag">
+<img src="{{ asset('/images/flag_france.png') }}" alt="France Flag">
     <h1>Hello From France !</h1>
     <p>It's currently {{ date('h:i A') }}.</p>
 @endsection
