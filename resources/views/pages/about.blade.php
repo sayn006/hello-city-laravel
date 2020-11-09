9beaead94f82cdb238550170360fc4e56f92fe25
sayn006 <sayn.punithan@gmail.com>
2020-11-09 14:05:04 +0100

    add images

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,6 +5,7 @@
 
 
 @section('content')
+<img src="/images/flag_france.png" alt="France Flag">
         <p>Built with &hearts; by Sayn.</p>
 <p><a href="{{ route('home') }}">Revenir à la page d'accueil</a></p>  
 @endsection
