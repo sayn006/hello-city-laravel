024f0665a62f4bf47f6b7831941a2946ae32dfa9
sayn006 <sayn.punithan@gmail.com>
2020-11-09 11:44:40 +0100

    remove duplication by using layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,23 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-        <title>About Us | Hello City</title>
-
-   
-    </head>
-    <body class="antialiased">
-        
-        
+@section('content')
         <p>Built with &hearts; by Sayn.</p>
-        <p><a href="/">Revenir à la page d'accueil</a></p>
-
-<footer>
-    <p>
-        &copy; Copyright {{ date('Y') }}</a>
-    </p>
-</footer>
-    </body>
-</html>
+        <p><a href="/">Revenir à la page d'accueil</a></p>  
+@endsection
